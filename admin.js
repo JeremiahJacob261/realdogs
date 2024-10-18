a36bab94d20f27dc43569b85d2c9422c6c33592f
Jeremiah <jeremiahjacob261@gmail.com>
2024-10-18 11:15:28 +0100

    admin 1.1.

--- admin.js
+++ admin.js
@@ -3,21 +3,84 @@ const TelegramBot = require('node-telegram-bot-api');
 const admintoken = '8172210694:AAFlCzfuhq31bwxFkxoKYQMXKpltuW0nb6A';
 const vbot = new TelegramBot(admintoken, { polling: true });
 
-const notifyadmin = async (user, address) =>{
-        vbot.sendMessage("6351462493",`${user} has placed a withdrawal\n\n The ton address is ${address}`)
+const notifyadmin = async (user, address) => {
+    const options = {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { text: '✅ DONE', callback_data: 'accept_withdrawal' },
+                    { text: '❌ DECLINE', callback_data: 'decline_withdrawal' },
+                    { text: '🔍 VIEW USER INFO', callback_data: 'see_user' }
+                ]
+            ]
+        }, parse_mode: 'HTML'
+    };
+
+    vbot.sendMessage("6351462493", `${user} has placed a withdrawal\n\n The ton address is ${address}`, options)
 }
 
 
 const starters = async () => {
-    
-vbot.onText(/\/start$/, (msg) => {
-    const chatId = msg.chat.id;
-    const username = msg.from.username;
-    vbot.sendMessage("1819258518", "HELLO Admin")
-          
-});
+
+    //start
+    vbot.onText(/\/start$/, (msg) => {
+        const chatId = msg.chat.id;
+        const username = msg.from.username;
+   try {
+    const mainMenuOptions = {
+        reply_markup: {
+            keyboard: [
+                ['USERS', 'WITHDRAWAL REQUEST', 'ANALYTICS', 'TASKS'],
+            ],
+            resize_keyboard: true,
+            one_time_keyboard: false
+        }
+        , parse_mode: 'HTML'
+    }
+    vbot.sendMessage(chatId, "HELLO Admin", mainMenuOptions);
+    console.log("start admin")
+   } catch (error) {
+    console.log(error)
+   }
+    });
+
+
+    //find user
+    vbot.onText("USERS", (msg) => {
+        const chatId = msg.chat.id;
+        const username = msg.from.username;
+        const mainMenuOptions = {
+            reply_markup: {
+                keyboard: [
+                    ['USERS', 'WITHDRAWAL REQUEST', 'ANALYTICS', 'TASKS'],
+                ],
+                resize_keyboard: true,
+                one_time_keyboard: false
+            }
+            , parse_mode: 'HTML'
+        }
+        vbot.sendMessage(chatId, "FIND USERS HERE", mainMenuOptions)
+    });
+
+    //tasks
+    vbot.onText("TASKS", (msg) => {
+        const chatId = msg.chat.id;
+        const username = msg.from.username;
+        const mainMenuOptions = {
+            reply_markup: {
+                keyboard: [
+                    ['MAIN MENU','ADD TASKS', 'DELETE TASKS', 'VIEW TASK'],
+                ],
+                resize_keyboard: true,
+                one_time_keyboard: false
+            }
+            , parse_mode: 'HTML'
+        }
+        vbot.sendMessage(chatId, "FIND USERS HERE", mainMenuOptions)
+    });
+
 }
 
 module.exports = {
-    notifyadmin,starters
+    notifyadmin, starters
 };
